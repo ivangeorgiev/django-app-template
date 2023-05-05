399d57fadb84251c368af8f4f69f7f58dbbc21c6
Ivan Georgiev <ivan.georgiev@nl.abnamro.com>
2023-05-05 13:02:59 +0200

    change app name from myapp to mydjangoapp for safer search and replace

--- tests/helper/djangoproject/settings.py
+++ tests/helper/djangoproject/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     "django_extensions",
     "debug_toolbar",
     # Local
+    "mydjangoapp",
 ]
 
 MIDDLEWARE = [
--- tests/test_hello_django.py
+++ tests/test_hello_django.py
@@ -1,6 +1,13 @@
 from django.urls import reverse
+import pytest
 
-def test_hello_django(client):
-    response = client.get(reverse("hello-django"))
+
+
+@pytest.fixture(name="hello_url")
+def given_hello_url() -> str:
+    return reverse("hello-django")
+
+def test_hello_django(client, hello_url):
+    response = client.get(hello_url)
     assert response.status_code == 200
     assert response.content == b'Hello, Django!'
